13d5858b2092c322ef75a57d4c3dc8ca037357b8
abhiyansh <abhiyansharya161@gmail.com>
2020-08-24 14:12:06 +0530

    decimal pt bug fixed

--- ReadData.cpp
+++ ReadData.cpp
@@ -1,19 +1,35 @@
 vector<string> input(string input){
 	vector<string> v;
-	string line, l;
+	string para, l;
 	ifstream file(input);
 
 	if(file.is_open()){
-		//read file one sentence at a time (delimiter used = '\n')
-		//adds each sentence (separated by '.') in vector of strings
-		while(getline(file, line, '\n')){
-			stringstream ss(line);
-        	while(getline(ss, l, '.')){
-             	if(l.size()>1){
-             		if(l[0]==' ') l = l.substr(1); 
-             		v.push_back(l);	
-             	}
-        	}
+		//read file one paragraph at a time (delimiter used = '\n')
+		//for each paragraph is tokenized (as a vector of strings) into sentences separated by full stops (.)
+		while(getline(file, para, '\n')){
+			int len = para.length();
+			//para with len>0
+			if(len){
+				//start index of a sentence
+				int start = 0;
+				//traversing the paragraph
+				for(int i=0;;i++){
+					//last character 
+					if(i==len-1){
+						if(para[i]!='.') v.push_back(para.substr(start, i-start+1));
+						else v.push_back(para.substr(start, i-start));
+						break;
+					}
+					//breaks sentences on full stops
+					if(para[i]=='.'){
+						//checks if '.' is a decimal pt
+						if(isdigit(para[i+1]) && isdigit(para[i-1])) continue;
+						v.push_back(para.substr(start, i-start));
+						start = (para[i+1] == ' ' ? i+2 : i+1);
+					}
+				}
+
+			}
 		}
 		file.close();
 	}
